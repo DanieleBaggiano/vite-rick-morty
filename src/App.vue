c417cb6fc3ed2ae435632fbe0d9ff18a640a4fd7
DanieleBaggiano <danielebaggiano@gmail.com>
2024-05-06 20:35:36 +0200

    inizio

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script>
+  import axios from "axios";
+  import AppHeader from '/src/components/AppHeader.vue';
+  import AppMain from '/src/components/AppMain.vue';
+
+  export default {
+    components: {
+      AppHeader,
+      AppMain,
+    },
+    data() {
+      return {
+        array: []
+      }
+    },
+    created() {
+      axios
+        .get("https://rickandmortyapi.com/api/character")
+        .then((resp) => {
+          this.array = resp.data.results
+          console.log(this.array);
+        })
+    }
+  }
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain :array="array" />
+
+  <div class="container">
+    <div class="grid" v-for="item in array">
+      <img :src="item.image" alt="">
+      <h3>{{ item.name }}</h3>
+      <span>{{ item.status }}</span> <br>
+      <span>{{ item.species }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 20px;
+    width: 80%
+  }
+
+  .grid {
+    text-align: center;
+    width: calc(100% / 4 - 20px);
+  }
+
+  img {
+    border-radius: 50%;
+    width: 200px;
+  }
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<template>
+    <h1>Rick and Morty App</h1>
+</template>
+
+<style scoped lang="scss">
+    @use "../style/partials/variables" as *;
+    @use "../style/partials/mixins" as *;
+
+    h1 {
+        text-align: center;
+        margin: 50px 0;
+    }
+</style>
